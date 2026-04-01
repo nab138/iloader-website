2db30afe94cb67741fddbd09975c501970d0fc80
nab138 <nab@nabdev.me>
2026-04-01 15:29:54 -0400

    Change to static html for better seo

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        pairing: 'pairing/index.html',
+        error: '404.html',
+      },
+    },
+  },
 })
